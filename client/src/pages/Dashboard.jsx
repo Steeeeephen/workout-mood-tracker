67a081d24808c0881dd76270bff82b0b458f790c
Stephen Zalalas <stephenzalalas@proton.me>
2026-02-12 19:58:39 -0500

    Rename original Dashboard component to Calendar (this is the weekly calendar) and create new Dashboard component. The new Dashboard will act as the home page when a user is logged in.

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,82 +1,8 @@
-import React, {useEffect, useState, useTransition} from 'react'
-import {useAuth} from "../context/AuthContext.jsx";
-import WeeklyView from "../components/WeeklyView.jsx";
-import axios from "axios";
-import {parse, parseISO} from "date-fns";
-import api from "../config/api.js";
-import EntryModal from "../components/EntryModal.jsx";
+import React from 'react'
 
 const Dashboard = () => {
-
-    const { user, loading } = useAuth();
-
-    const [ entries, setEntries ] = useState([]);
-
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [editingEntry, setEditingEntry] = useState(null);
-
-    const handleSuccess = () => {
-        setIsModalOpen(false);
-        setEditingEntry(null);
-        fetchEntries()
-    }
-
-
-    useEffect(() => {
-        // Setting page title
-        if (user?.first_name){
-            document.title = `${user?.first_name}'s Dashboard`;
-        } else {
-            document.title = 'Dashboard';
-        }
-    }, [user]);
-
-    const fetchEntries = async () => {
-        try {
-            const token = localStorage.getItem('token');
-            const response = await axios.get('http://localhost:3000/api/entries', {
-                headers: {
-                    'Authorization': `Bearer ${token}`
-                }
-            });
-            setEntries(response.data);
-        } catch (err){
-            console.error(err);
-        }
-    }
-
-    useEffect(() => {
-        if (user) fetchEntries()
-    }, [user]);
-
-
-
-
     return (
-        <div className="w-3/5 m-auto">
-
-            {isModalOpen && (
-                <EntryModal
-                    entry={null}
-                    onClose={() => setIsModalOpen(false)}
-                    onSuccess={handleSuccess}
-                />
-
-            )}
-
-            <title>{user?.first_name}'s Dashboard</title>
-            {/*<h1 className="text-3xl font-bold">Hi, {user?.first_name}</h1>*/}
-            <WeeklyView entries={entries}/>
-
-            <div className="flex flex-row-reverse">
-                <button
-                    className="bg-green-400 rounded cursor-pointer p-3 mt-3"
-                    onClick={()=>setIsModalOpen(true)}
-                >Create Entry
-                </button>
-            </div>
-        </div>
-
+        <div>Dashboard</div>
     )
 }
 export default Dashboard
