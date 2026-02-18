eb4b811d58c31353c074de2905d0dadf55de9e02
Stephen Zalalas <stephenzalalas@proton.me>
2026-02-18 18:33:26 -0500

    Change login function to navigate to '/' instead of '/dashboard'

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,73 +1,79 @@
-import React, {useState} from 'react'
-import api from '../config/api.js'
+import React, { useState } from 'react';
+import api from '../config/api.js';
 import { useNavigate } from 'react-router-dom'; // Add this
 import { useAuth } from '../context/AuthContext.jsx'; // Add this
 import { useNotification } from '../context/NotificationContext.jsx';
 
 const Login = () => {
+  const { showError, showSuccess } = useNotification();
 
-    const { showError, showSuccess } = useNotification();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
-    const [loading, setLoading] = useState(false);
+  const { login } = useAuth();
+  const navigate = useNavigate();
 
-    const { login } = useAuth();
-    const navigate = useNavigate();
-
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        setError('');
-        setLoading(true)
-        try {
-            await login(email, password);
-            console.log('Login worked!');
-            showSuccess('Welcome back!');
-            navigate('/dashboard');
-
-
-        } catch (err) {
-            showError('Invalid email or password');
-        } finally {
-            setLoading(false)
-        }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError('');
+    setLoading(true);
+    try {
+      await login(email, password);
+      console.log('Login worked!');
+      showSuccess('Welcome back!');
+      navigate('/');
+    } catch (err) {
+      showError('Invalid email or password');
+    } finally {
+      setLoading(false);
     }
+  };
 
-    return (
-        <form onSubmit={handleSubmit} action="" className="login-forms" >
-            <h3 className="text-2xl mb-10 text-center">Login</h3>
+  return (
+    <form onSubmit={handleSubmit} action="" className="login-forms">
+      <h3 className="text-2xl mb-10 text-center">Login</h3>
 
-            <label htmlFor="email" className="sr-only">Email</label>
-            <input
-                type="email"
-                name="email"
-                id="login-email"
-                placeholder='Email'
-                className="text-input border-b-2  p-1"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-            />
+      <label htmlFor="email" className="sr-only">
+        Email
+      </label>
+      <input
+        type="email"
+        name="email"
+        id="login-email"
+        placeholder="Email"
+        className="text-input border-b-2  p-1"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
 
-            <label htmlFor="password" className="sr-only">Password</label>
-            <input
-                type="password"
-                name="password"
-                id="login-password"
-                placeholder="Password"
-                className="text-input mb-10 border-b-2  p-1"
-                value={password}
-                onChange={(e)=> setPassword(e.target.value)}
-            />
+      <label htmlFor="password" className="sr-only">
+        Password
+      </label>
+      <input
+        type="password"
+        name="password"
+        id="login-password"
+        placeholder="Password"
+        className="text-input mb-10 border-b-2  p-1"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
 
-            {error && <p style={{ color: 'red' }}>{error}</p>}
+      {error && <p style={{ color: 'red' }}>{error}</p>}
 
-            <button type="submit" className="btn-submit bg-green-400 rounded p-1">Login</button>
-            <span className="m-auto pt-10">Forgot password? Click <a className="text-blue-600" href="#">here</a>.</span>
-        </form>
-    )
-}
-export default Login
-
-
+      <button type="submit" className="btn-submit bg-green-400 rounded p-1">
+        Login
+      </button>
+      <span className="m-auto pt-10">
+        Forgot password? Click{' '}
+        <a className="text-blue-600" href="#">
+          here
+        </a>
+        .
+      </span>
+    </form>
+  );
+};
+export default Login;
