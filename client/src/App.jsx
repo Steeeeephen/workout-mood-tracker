14a373060a42656e946ab3feaba245502ca13984
Stephen Zalalas <stephenzalalas@proton.me>
2026-02-19 17:05:13 -0500

    Add footer.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,40 +1,54 @@
-
-import './App.css'
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import Home from "./pages/Home.jsx";
-import Navbar from "./components/Navbar.jsx";
-import Calendar from "./pages/Calendar.jsx";
-import {AuthProvider} from "./context/AuthContext.jsx";
-import ProtectedRoute from "./components/ProtectedRoute.jsx";
-import DayView from "./pages/DayView.jsx";
-import Register from "./pages/Register.jsx";
-import Login from "./pages/Login.jsx";
-import NotificationToast from "./components/NotificationToast.jsx";
-import {NotificationProvider} from "./context/NotificationContext.jsx";
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import Navbar from './components/Navbar.jsx';
+import Calendar from './pages/Calendar.jsx';
+import { AuthProvider } from './context/AuthContext.jsx';
+import ProtectedRoute from './components/ProtectedRoute.jsx';
+import DayView from './pages/DayView.jsx';
+import Register from './pages/Register.jsx';
+import Login from './pages/Login.jsx';
+import NotificationToast from './components/NotificationToast.jsx';
+import { NotificationProvider } from './context/NotificationContext.jsx';
+import Footer from './components/Footer.jsx';
 
 function App() {
-    console.log('rendering')
-
+  console.log('rendering');
 
   return (
-      <div className="min-h-screen flex flex-col">
-          <NotificationProvider>
-              <AuthProvider>
-                   <BrowserRouter>
-                        <Navbar />
-                        <NotificationToast />
-                          <Routes>
-                              <Route path="/" element={<Home />}/>
-                              <Route path="/calendar" element={<ProtectedRoute><Calendar /></ProtectedRoute>}/>
-                              <Route path="/day/:date" element={<ProtectedRoute><DayView /></ProtectedRoute>} />
-                              <Route path="/register" element={<Register />} />
-                              <Route path="/login" element={<Login />} />
-                          </Routes>
-                    </BrowserRouter>
-              </AuthProvider>
-          </NotificationProvider>
-      </div>
-  )
+    <div className="min-h-screen flex flex-col">
+      <NotificationProvider>
+        <AuthProvider>
+          <BrowserRouter>
+            <Navbar />
+            <NotificationToast />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route
+                path="/calendar"
+                element={
+                  <ProtectedRoute>
+                    <Calendar />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/day/:date"
+                element={
+                  <ProtectedRoute>
+                    <DayView />
+                  </ProtectedRoute>
+                }
+              />
+              <Route path="/register" element={<Register />} />
+              <Route path="/login" element={<Login />} />
+            </Routes>
+            <Footer />
+          </BrowserRouter>
+        </AuthProvider>
+      </NotificationProvider>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const Footer = () => {
+  return <div className="flex justify-center m-4">Footer</div>;
+};
+export default Footer;
