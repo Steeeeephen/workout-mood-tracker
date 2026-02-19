375aa05cefcc9f0a1534a2a28ae3ce03bf732e5e
Stephen Zalalas <stephenzalalas@proton.me>
2026-02-18 21:09:59 -0500

    Style Login page and add image from undraw.

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -32,52 +32,61 @@ const Login = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit} action="" className="login-forms">
-      <h3 className="text-2xl mb-10 text-center">Login</h3>
+    <>
+      <div className="mt-24 mx-auto px-8 flex items-center gap-16 max-w-6xl">
+        <div className="">
+          <img src="/undraw_fitness-stats_bd09.svg" alt="" />
+        </div>
 
-      <label htmlFor="email" className="sr-only">
-        Email
-      </label>
-      <input
-        type="email"
-        name="email"
-        id="login-email"
-        placeholder="Email"
-        className="text-input border-b-2  p-1"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
+        <form onSubmit={handleSubmit} action="" className="login-forms flex-1">
+          <h1 className="text-6xl font-extrabold mb-10 text-center">
+            Welcome Back!
+          </h1>
+          <label htmlFor="email" className="sr-only">
+            Email
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="login-email"
+            placeholder="Email"
+            className="text-input border-b-2  p-1"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
 
-      <label htmlFor="password" className="sr-only">
-        Password
-      </label>
-      <input
-        type="password"
-        name="password"
-        id="login-password"
-        placeholder="Password"
-        className="text-input mb-10 border-b-2  p-1"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
+          <label htmlFor="password" className="sr-only">
+            Password
+          </label>
+          <input
+            type="password"
+            name="password"
+            id="login-password"
+            placeholder="Password"
+            className="text-input mb-10 border-b-2  p-1"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
-      {error && <p style={{ color: 'red' }}>{error}</p>}
+          {error && <p style={{ color: 'red' }}>{error}</p>}
 
-      <button
-        type="submit"
-        className="btn-submit bg-green-400 rounded p-1"
-        disabled={isLoggingIn}
-      >
-        {isLoggingIn ? 'Logging in...' : 'Log In'}
-      </button>
-      <span className="m-auto pt-10">
-        Forgot password? Click{' '}
-        <a className="text-blue-600" href="#">
-          here
-        </a>
-        .
-      </span>
-    </form>
+          <button
+            type="submit"
+            className="btn-submit bg-green-400 rounded p-1"
+            disabled={isLoggingIn}
+          >
+            {isLoggingIn ? 'Logging in...' : 'Log In'}
+          </button>
+          <span className="m-auto pt-10">
+            Forgot password? Click{' '}
+            <a className="text-blue-600" href="#">
+              here
+            </a>
+            .
+          </span>
+        </form>
+      </div>
+    </>
   );
 };
 export default Login;
