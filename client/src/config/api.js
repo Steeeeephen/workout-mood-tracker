f8443f2baca787e6a8a5a53c563f1dcd85788f03
Stephen Zalalas <stephenzalalas@proton.me>
2026-02-26 13:22:14 -0500

    Add comments to api.js

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,10 +1,15 @@
 import axios from 'axios';
 
+// This creates a custom instance of Axios to use throughout the app for api calls.
+// It's of course called 'api'.
 const api = axios.create({
+  // VITE_API_URL is defined in the Vercel settings
   baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use(
+  // I added an interceptor to the api to intercept the config object (request) and check for jwt before it is sent.
+  // This automatically attaches the JWT to every request, so the backend can verify the user is authenticated.
   (config) => {
     const token = localStorage.getItem('token');
     if (token) {
